d6f9974273585ab543f06366d6c31916488b4c17
sammorton11 <sammorton11@yahoo.com>
2023-08-03 10:00:55 -0500

    Fixing netlify forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ export default function Contact() {
       body: encode({"form-name": "contact", name, email, message}),
     })
     .then(() => alert("Message sent!"))
-    .catch((error) => alert(error)); 
+    .catch((error) => alert(error));
   }
 
   return (
@@ -68,10 +68,10 @@ export default function Contact() {
           </div>
         </div>
         <form
-          netlify
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-          onSubmit={handleSubmit}>
+          onSubmit={handleSubmit}
+          netlify>
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
