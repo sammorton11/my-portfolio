e002083250a041a88954b873f34b69606ece2de8
sammorton11 <sammorton11@yahoo.com>
2023-09-30 20:40:54 -0500

    updated about me and added a new project also fixed the project list covering the navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar/>
       <About/>
       <Projects/>
       <Skills/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,22 +11,22 @@ export default function Navbar() {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+          <a href="#projects" className="text-purple-100 mr-5">
             Past Work
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a href="#skills" className="mr-5 text-purple-100">
             Skills
           </a>
-          <a href="https://www.linkedin.com/in/samuel-morton-a7b82a232/" className="mr-5 hover:text-white" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/samuel-morton-a7b82a232/" className="mr-5 text-purple-100" target="_blank" rel="noopener noreferrer">
             LinkedIn
           </a>
-          <a href="https://github.com/sammorton11" className="mr-5 hover:text-white" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/sammorton11" className="mr-5 text-purple-100" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0" target="_blank" rel="noopener noreferrer">
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 text-purple-100" target="_blank" rel="noopener noreferrer">
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
